3e5b5fcf63d573f1dbcb0100b546d715cbb5e572
Ralf Eggert <ralf@travello.com>
2014-12-27 08:18:17 +0100

    Bootstrap Form supports Buttons and Checkboxes now

--- src/ZF2rapidLib/View/Helper/BootstrapForm.php
+++ src/ZF2rapidLib/View/Helper/BootstrapForm.php
@@ -13,6 +13,8 @@
  */
 namespace ZF2rapidLib\View\Helper;
 
+use Zend\Form\Element\Button;
+use Zend\Form\Element\Checkbox;
 use Zend\Form\Element\Csrf;
 use Zend\Form\Element\Hidden;
 use Zend\Form\Element\Submit;
@@ -46,20 +48,32 @@ class BootstrapForm extends AbstractHelper
         $output = $this->getView()->form()->openTag($form);
 
         foreach ($form as $element) {
-            if ($element instanceof Submit) {
+            if ($element instanceof Submit || $element instanceof Button) {
                 $submitElements[] = $element;
-            } elseif ($element instanceof Csrf
-                || $element instanceof Hidden
-            ) {
+            } elseif ($element instanceof Csrf || $element instanceof Hidden) {
                 $output .= $this->getView()->formElement($element);
+            } elseif ($element instanceof Checkbox) {
+                // setup view model
+                $viewModel = new ViewModel();
+                $viewModel->setVariable('element', $element);
+                $viewModel->setTemplate(
+                    'zf2rapid-library/widget/bootstrap-form-checkbox'
+                );
+
+                // render form element
+                $output .= $this->getView()->render($viewModel);
             } else {
                 $element->setAttributes(array('class' => 'form-control'));
-                $element->setLabelAttributes(array('class' => 'col-sm-2 control-label'));
+                $element->setLabelAttributes(
+                    array('class' => 'col-sm-2 control-label')
+                );
 
                 // setup view model
                 $viewModel = new ViewModel();
                 $viewModel->setVariable('element', $element);
-                $viewModel->setTemplate('zf2rapid-library/widget/bootstrap-form-group');
+                $viewModel->setTemplate(
+                    'zf2rapid-library/widget/bootstrap-form-group'
+                );
 
                 // render form element
                 $output .= $this->getView()->render($viewModel);
@@ -70,7 +84,9 @@ class BootstrapForm extends AbstractHelper
         // setup view model
         $viewModel = new ViewModel();
         $viewModel->setVariable('submitElements', $submitElements);
-        $viewModel->setTemplate('zf2rapid-library/widget/bootstrap-form-submit');
+        $viewModel->setTemplate(
+            'zf2rapid-library/widget/bootstrap-form-submit'
+        );
 
         // render submit elements
         $output .= $this->getView()->render($viewModel);
